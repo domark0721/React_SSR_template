8f18ff715686c6463d72a94f9baf7be6913b171a
Mark Tsai <mark.tsai@langlive.com>
2018-12-10 17:04:32 +0800

    rename and adjust react-hot-loader style

--- src/index.js
+++ src/index.js
@@ -1,35 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { AppContainer } from 'react-hot-loader'
+// import { AppContainer } from 'react-hot-loader'
 
 import store from './store'
 
 import App from './components/App/App'
 
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById('root'),
-// )
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
 
-const render = (Component) => {
-  ReactDOM.render(
-    <AppContainer>
-      <Provider store={store}>
-        <Component />
-      </Provider>
-    </AppContainer>,
-    document.getElementById('root'),
-  )
-}
+// const render = (Component) => {
+//   ReactDOM.render(
+//     <AppContainer>
+//       <Provider store={store}>
+//         <Component />
+//       </Provider>
+//     </AppContainer>,
+//     document.getElementById('root'),
+//   )
+// }
 
-render(App)
+// render(App)
+// const root = require('./components/App/App').default
 
-if (module.hot) {
-  module.hot.accept(
-    './components/App/App',
-    () => render(App), // if you are using harmony module ({ modules: false })
-  )
-}
+// if (module.hot) {
+//   module.hot.accept(
+//     './components/App/App',
+//     () => {
+//       const NextRootContainer = root
+//       render(NextRootContainer)
+//     }, // if you are using harmony module ({ modules: false })
+//   )
+// }
