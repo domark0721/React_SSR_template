9894ad092892f22f9394af701853c933c0af6dbb
Ryan Lyu <ryan.lyu@langlive.com>
2018-12-13 14:18:17 +0800

    remove react-hot-loader

--- src/client.js
+++ src/client.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
-// import { AppContainer } from 'react-hot-loader'
 
 import configureStore from './store'
 
