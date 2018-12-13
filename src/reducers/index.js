abffcc7616b95f959f7dc5052257045d3111e39b
Mark Tsai <mark.tsai@langlive.com>
2018-12-13 19:16:03 +0800

    add example reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import commonReducer from './commonReducer'
+import exampleReducer from './exampleReducer'
 
 export default combineReducers({
   commonReducer,
+  exampleReducer,
 })
